89c8071e02dadfcc3da899efeb3c7eb42e771740
AKBarcenas <akbarcen@ucsd.edu>
2016-01-01 07:19:34 -0800

    Set up storyboard

---
+++ Names to Faces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Alex on 1/1/16.
+//  Copyright © 2016 Alex Barcenas. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
