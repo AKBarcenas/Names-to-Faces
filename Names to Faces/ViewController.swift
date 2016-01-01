22786f6413ca7e057df5fd24d24e312781d8fd68
AKBarcenas <akbarcen@ucsd.edu>
2016-01-01 06:56:04 -0800

    Initial Commit

---
+++ Names to Faces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Names to Faces
+//
+//  Created by Alex on 1/1/16.
+//  Copyright © 2016 Alex Barcenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
